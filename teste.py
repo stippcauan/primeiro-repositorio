cd7ee7371991a2bf101ee70870613c25c9832568
stippcauan <cauan.stipp@escola.pr.gov.br>
2025-05-22 14:51:52 +0000

    exercicio02

--- teste.py
+++ teste.py
@@ -1,1 +1,8 @@
-print("olá mundo")
+curso01 = int(input("digite qual curso teve mais avaliações o curso01 teve mais avaliações"))
+curso02 = int(input("digite qual curso teve mais avaliações o curso02 teve mais avaliações"))
+if curso01 == curso02:
+     print("empate")
+elif curso01 > curso02:
+       print("curso01 é maior")
+else:
+       print("curso02 é maior")
